abe038cd6a5f13c1088984f2201875636fa2695b
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-13 23:58:14 -0400

    eslinted fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -3,22 +3,22 @@
 // Downloaded and saved 3261 bytes to ./index.html
 
 //fetch inputted data
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 
 
 const uRL = args[0];
 const localPath = args[1];
 
 //use fs and request
-const fs = require('fs')
+const fs = require('fs');
 const request = require('request');
 
-request(`${uRL}`, (error, response, body) => { 
+request(`${uRL}`, (error, response, body) => {
   fs.writeFile(localPath, body, err => {
-    if(err) {
+    if (err) {
       console.error(err);
-      return
+      return;
     }
-    console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`)
-  })
- })
+    console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`);
+  });
+});
